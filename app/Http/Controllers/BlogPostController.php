2f4d5da950236d32d5702b15eec013601e860ba0
JSvaeva <e.svaeva1@gmail.com>
2021-10-02 08:22:25 +0400

    Add standard request-response rules

--- app/Http/Controllers/BlogPostController.php
+++ app/Http/Controllers/BlogPostController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-//use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Validator;
 use App\Models\BlogPost;
 use App\Models\User;
 
@@ -16,18 +17,24 @@ class BlogPostController extends Controller
      */
     public function index($type, $field)
     {
-        if (($type != 'asc' && $type != 'desc') || ($field != 'created' && field != 'updated')) {
-            abort(404);
+        if ($type != 'asc' && $type != 'desc') {
+            $type = 'asc';
         }
 
-        $blogPosts = BlogPost::orderBy($field.'_at', $type)->get();
-        $blogPosts = $blogPosts->simplePaginate(10);
+        if ($field != 'created' && field != 'updated') {
+            $field = 'updated';
+        }
         
+        $perPage = 15;
+
+        $blogPosts = BlogPost::orderBy($field.'_at', $type)->get();
         $blogPosts = $blogPosts->map(function ($item, $key) {
             return collect($item)->except(['created_at', 'updated_at'])->toArray();
         });
 
-        return $blogPosts;
+        $blogPosts = $blogPosts->paginate($perPage);
+
+        return response()->json(['data' => $blogPost], 200);
     }
 
     /**
@@ -38,18 +45,40 @@ class BlogPostController extends Controller
      */
     public function store(Request $request) //admin
     {
-        $is_admin = Auth::user()->is_admin;
-        if (!$is_admin) {
-            abort(404);
+        if (!Auth::check()) {
+            return response()->json([
+                'http_code' => 401,
+                'code' => 1, 
+                'title' => 'Log In Error',
+                'message' => 'You must be logged in to view this page'
+            ], 401);
         }
 
-        $request->validate([
+        if (!Auth::user()->is_admin) {
+            return response()->json([
+                'http_code' => 403,
+                'code' => 1, 
+                'title' => 'Access Error',
+                'message' => 'You don\'t have the access to this page'
+            ], 403);
+        }
+
+        $validator = Validator::make($request->all(), [
             'title' => 'required|max:100',
             'content' => 'required|min:10|max:5000',
             'author_id' => 'required'
         ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'http_code' => 422,
+                'code' => 1, 
+                'title' => 'Validation Error',
+                'message' => $validator->messages()
+            ], 422);
+        }
 
-        return BlogPost::create($request->all());
+        $blogPost = BlogPost::create($request->all());
+        return response()->json(['data' => $blogPost], 201);
     }
 
     /**
@@ -60,7 +89,8 @@ class BlogPostController extends Controller
      */
     public function show($id)
     {
-        return BlogPost::find($id)->except(['created_at', 'updated_at']);
+        $blogPost = BlogPost::find($id)->except(['created_at', 'updated_at']);
+        return response()->json(['data' => $blogPost], 202);
     }
 
     /**
@@ -72,21 +102,42 @@ class BlogPostController extends Controller
      */
     public function update(Request $request, $id) //admin
     {
-        $is_admin = Auth::user()->is_admin;
-        if (!$is_admin) {
-            abort(404);
+        if (!Auth::check()) {
+            return response()->json([
+                'http_code' => 401,
+                'code' => 1, 
+                'title' => 'Log In Error',
+                'message' => 'You must be logged in to view this page'
+            ], 401);
         }
 
-        $request->validate([
+        if (!Auth::user()->is_admin) {
+            return response()->json([
+                'http_code' => 403,
+                'code' => 1, 
+                'title' => 'Access Error',
+                'message' => 'You don\'t have the access to this page'
+            ], 403);
+        }
+
+        $validator = Validator::make($request->all(), [
             'title' => 'required|max:100',
             'content' => 'required|min:10|max:5000',
             'author_id' => 'required'
         ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'http_code' => 422,
+                'code' => 1, 
+                'title' => 'Validation Error',
+                'message' => $validator->messages()
+            ], 422);
+        }
 
         $blogPost = BlogPost::find($id);        
         $blogPost->update($request->all());
     
-        return $blogPost;
+        return response()->json(['data' => $blogPost], 202);
     }
 
     /**
@@ -97,16 +148,41 @@ class BlogPostController extends Controller
      */
     public function destroy($id) //admin
     {
-        $is_admin = Auth::user()->is_admin;
-        if (!$is_admin) {
-            abort(404);
+        if (!Auth::check()) {
+            return response()->json([
+                'http_code' => 401,
+                'code' => 1, 
+                'title' => 'Log In Error',
+                'message' => 'You must be logged in to view this page'
+            ], 401);
         }
 
-        return Post::destroy($id);
+        if (!Auth::user()->is_admin) {
+            return response()->json([
+                'http_code' => 403,
+                'code' => 1, 
+                'title' => 'Access Error',
+                'message' => 'You don\'t have the access to this page'
+            ], 403);
+        }
+
+        $blogPost = BlogPost::find($id);
+        $blogPost->delete();
+
+        return response()->json(['data' => [ 'id' => $id ]], 203);
     }
 
     public function author()
     {
-        return $this->belongsTo(User::class);
+        if (!Auth::check()) {
+            return response()->json([
+                'http_code' => 401,
+                'code' => 1, 
+                'title' => 'Log In Error',
+                'message' => 'You must be logged in to view this page'
+            ], 401);
+        }
+
+        return response()->json(['data' => $this->belongsTo(User::class) ], 200);
     }
 }
